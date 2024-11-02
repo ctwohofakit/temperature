b595e791aad0d49f0a4da1c826c7975e45aa1136
ctwohofakit <ctwohofa@gmail.com>
2024-11-02 14:46:29 -0700

    my first varible

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+console.log("script")
+//strings
+let myName="Kit";//declare and assign value
+let lastName="Sitou";
+//boolean values
+let isTeacher = false;
+//numerical values
+let mySize=169;
+var myAge=99;
+
+//display the values on the console
+console.log(myName);
+console.log(myAge,mySize,isTeacher,lastName);
+
+//concatenation
+console.log("My name is "+ myName);
+console.log("My name is "+ myName+", I am "+ myAge +" years old and my size is " + mySize + ".")
+
+//operation
+let num1 = 10;
+let num2 = 2;
+//sub,dev,mult
+let addition = num1 + num2;
+console.log("addition is: "+ addition);
+
+//sub
+let sub = num1 - num2;
+console.log("addition is: "+ sub);
+//dev
+let dev = num1 / num2;
+console.log("addition is: "+ dev);
+//mult
+let mult = num1 * num2;
+console.log("addition is: "+ mult);
+
+const pi = 3.1416;
+let r = 3;
+let area =pi*r**2;//**is square/
+console.log("area =" + area)
+
+
+
+
+
+
+
+
+//character
+//action
+//object
+//place
+//time
